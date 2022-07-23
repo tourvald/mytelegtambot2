0af3696d3804f6c60ee1676491266432ee8c9d88
dmitry <sw.newgeneration@icloud.com>
2022-07-23 11:12:30 +0300

    22.07.22 Бот определяет под какой системой работает и в зависимости от этого подставляет нужный токен. Что бы не нужно было редактировать токен при перебросе бота на сервак и обратно

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
 from dotenv import load_dotenv
-
+import platform
 load_dotenv()
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+print (platform.processor())
+if platform.processor() == 'Intel64 Family 6 Model 42 Stepping 7, GenuineIntel':
+	BOT_TOKEN = os.getenv("BOT_TOKEN_WIN")
+else:
+	BOT_TOKEN = os.getenv("BOT_TOKEN")
--- test.py
+++ test.py
@@ -1,16 +1,6 @@
 import os
 from dotenv import load_dotenv
 
-from sys import platform
-print (platform)
+import platform
+print (platform.node())
 
-if platform == "linux" or platform == "linux2":
-    print (platform)
-elif platform == "darwin":
-    print (platform)
-elif platform == "win32":
-    print (platform)
-
-load_dotenv()
-PATH_TO_WEBDRIVER_MAC = os.getenv("PATH_TO_WEBDRIVER_MAC")
-print(PATH_TO_WEBDRIVER_MAC)

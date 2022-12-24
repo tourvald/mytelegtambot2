734d411336f839bbaa30bcd0e83c1502f8499372
dmitrijkozuskevic <sw.newgeneration@icloud.com>
2022-12-24 16:20:19 +0300

    Добавлена функция автоматического запроса mycars каждый час. И запись данных в файл cars.csv

--- archive.py
+++ archive.py
@@ -151,10 +151,7 @@ def archive_status():
 
 
 if __name__ == '__main__':
-    # req = 'iphone 12 pro max 128'
-    # x = get_price_history(req)
-    # for y in x:
-    #     print (y)
+    print(archive_status())
     print(archive_status())
 
 
